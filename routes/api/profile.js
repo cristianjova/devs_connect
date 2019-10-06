5d6dc3f8a8693941fae9a87f805f0059931aac55
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-10-06 16:56:52 -0300

    get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route     GET api/profile
-// @desc      Test route
-// @access    Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route     GET api/profile/me
+// @desc      Get current users profile
+// @access    Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    // Create profile variable and add fields from user model
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avater']
+    );
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'Este usuario no tiene un perfil asociado' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
