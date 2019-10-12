90f174c54d11ec3332a4147e3a22203137714919
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-10-12 02:13:20 -0300

    add profile experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -168,4 +168,64 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route     PUT api/profile/experience
+// @desc      Add profile experience
+// @access    Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'El campo posición es obligatorio')
+        .not()
+        .isEmpty(),
+      check('company', 'El campo compañia es obligatorio')
+        .not()
+        .isEmpty(),
+      check('from', 'El campo desde es obligatorio')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
