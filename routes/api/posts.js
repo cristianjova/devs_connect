0b89cb28c633e33779bba4ba9bde561917002d7b
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-10-14 02:46:36 -0300

    get & delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,8 +8,8 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route     POST api/posts
-// @desc      Crate a post
-// @access    PRivate
+// @desc      Create a post
+// @access    Private
 router.post(
   '/',
   [
@@ -45,5 +45,67 @@ router.post(
     }
   }
 );
+
+// @route     GET api/posts
+// @desc      Get all posts
+// @access    Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); // Most recent
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     GET api/posts/:id
+// @desc      Get post by id
+// @access    Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Publicación no encontrada' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Publicación no encontrada' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     DELETE api/posts/:id
+// @desc      Delete post
+// @access    Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Publicación no encontrada' });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Usuario no autorizado' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Publicación Eliminada' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Publicación no encontrada' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
