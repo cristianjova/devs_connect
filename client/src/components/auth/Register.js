044ed14faaeac63840ab1737f76a542832418946
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-10-24 07:41:22 -0300

    Register form & useState hook

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Registrate</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Crea tu cuenta
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Nombre'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Correo Electronico'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className='form-text'>
+            Este sitio usa Gravatar, si quieres tener una imagen de perfil, usa
+            un correo asociado a Gravatar
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Contraseña'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirma Contraseña'
+            name='password2'
+            value={password2}
+            onChange={e => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' value='Registrarse' className='btn btn-primary' />
+      </form>
+      <p className='my-1'>
+        ¿Ya tienes una cuenta? <a href='login.html'>Inicia Sesion</a>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
