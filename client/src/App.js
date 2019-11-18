a3b607dcb8cd226ac5e95665f1fab608102f51ba
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-11-18 19:19:13 -0300

    protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,13 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
@@ -34,6 +37,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact pat='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
